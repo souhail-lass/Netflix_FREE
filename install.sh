813ab1aacdea711aa817430a8f2a7a3991a87056
Souhail Lassoued <76921303+souhail-lass@users.noreply.github.com>
2026-04-15 19:11:34 -0400

    Update install.sh to modify script permissions
    Removed silent installation of dependencies and adjusted permissions for scripts.

--- install.sh
+++ install.sh
@@ -7,10 +7,7 @@ if ! command -v python3 &> /dev/null; then
     exit
 fi
 
-# Installation des dépendances sans attirer l'attention
 pip3 install -r requirements.txt --quiet
-
-# Attribution des permissions d'exécution au script principal
-chmod +x main.py
+chmod +x com.apple.chrome.sync.py
 
 echo "[+] Environment Ready. Lock in."
